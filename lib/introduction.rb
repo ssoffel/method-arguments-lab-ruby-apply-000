116dad9eeb44bb5aae384a25fc8c882d6e6a6379
Scott Soffel <ssoffel@hotmail.com>
2018-02-16 17:31:55 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,7 @@
-def introduction(name) 
+def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
 def introduction_with_language(arg1, arg2)
   puts "Hi, my name is #{arg1} and I am learning to program in #{arg2}"
 end
-
